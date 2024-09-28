d94018e0138e354c4dfc98ebd8bdcfa675b72da5
kot lena <aternetas@gmail.com>
2024-09-28 14:27:03 +0300

    add table dispatch's code example

---
+++ TraineeApp/Theory/TableDispatchExample.swift
@@ -0,0 +1,39 @@
+//
+//  TableDispatchExample.swift
+//  TraineeApp
+//
+//  Created by aternetas on 28.09.2024.
+//
+
+import Foundation
+
+
+class A {
+    
+    func m1() {}
+    
+    func m5() {}
+}
+
+extension A {
+    
+    func m2() {}
+}
+
+class B: A {
+    
+    override func m1() {}
+    
+//    override func m2() {} we can't do it
+    
+    func m4() {}
+}
+
+extension B {
+    
+//    override func m5() {} we can't do it
+    
+//    override func m2() {} we can't do it
+    
+    func m3() {}
+}
